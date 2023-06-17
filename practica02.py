123fc4b76d4518015a86d2f8cf851463fd72ae4f
Angel Chuchullo Chacca <achuchullo@unsa.edu.pe>
2023-06-17 15:04:17 -0600

    se creo 2 ejerecios sobre regla de tres simples, el segundo ejercicio trata sobre promedios

---
+++ practica02.py
@@ -0,0 +1,6 @@
+print("esta usando le regla de tres simple inversante proporcional")
+dato=float(input("ingrese su dato1 de arriba de tu lado derecho: "))
+dato1=float(input("ingrese su dato2 de arriba de tu lado izquierdo: "))
+dato2=float(input("ingrese su dato3 de abajo de tu lado derecho: "))
+resultado=dato2*dato1/dato
+print("su resultado es: ",resultado)
---
+++ practica03.py
@@ -0,0 +1,7 @@
+print("operacion promedios")
+nota1=float(input("ingrese su primera nota: "))
+nota2=float(input("ingrese su segunda nota: "))
+nota3=float(input("ingrese su tercera nota: "))
+nota4=float(input("ingrese su cuarta nota: "))
+resultado=(nota1+nota2+nota3+nota4)/4
+print("el promedio es: ",resultado)
